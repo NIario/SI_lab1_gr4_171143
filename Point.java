8f1ee55b6c07e0a6cb57644793f09517769b207d
NIario <63304806+NIario@users.noreply.github.com>
2020-04-09 20:37:04 +0200

    last commit

--- Point.java
+++ Point.java
@@ -1,58 +1,77 @@
-class Point {
-	String id
-	double x,y;
-    String color;
+import java.util.*;
 
-	public Point(String id, double x, double y, String color){
-		this.id = id;
-		this.x = x;
-		this.y = y;
-		this.color = color;
+public class Point {
+	    String id;
+		double x, y;
+		String color;
+
+		public Point(String id, double x, double y, String color) {
+			this.id = id;
+			this.x = x;
+			this.y = y;
+			this.color = color;
+		}
+
+		public void setColor(String color) {
+			this.color = color;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public void setX(double x) {
+			this.x = x;
+		}
+
+		public void setY(double y) {
+			this.y = y;
+		}
+
+		public double getX() {
+			return x;
+		}
+
+		public double getY() {
+			return y;
+		}
+
+		public String getColor() {
+			return color;
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void move(Character xDirection, Character yDirection) {
+
+			if (xDirection.equals("L"))
+				x--;
+			else if (xDirection.equals("R"))
+				x++;
+			if (yDirection.equals("U"))
+				y++;
+			else if (yDirection.equals("D"))
+				y--;
+		}
+
+		public void draw() {
+			System.out.println("Kordinati na tockata se X: " + x + ", Y: " + y);
+		}
 	}
 
-	public void setColor(String color) {
-		this.color = color;
-	}
+	class Canvas {
+		List<Point> points;
 
-	public void setId(String id) {
-		this.id = id;
-	}
+		public Canvas(List<Point> points) {
+			this.points = points;
+		}
 
-	public void setX(double x) {
-		this.x = x;
+		void add(Point newpoint){
+			points.add(newpoint);
+		}
+		void remove(int index){
+			points.remove(index);
+		}
 	}
-
-	public void setY(double y) {
-		this.y = y;
-	}
-
-	public double getX() {
-		return x;
-	}
-
-	public double getY() {
-		return y;
-	}
-
-	public String getColor() {
-		return color;
-	}
-
-	public String getId() {
-		return id;
-	}
-	public void move (char xDirection, char yDirection) {
-		if(xDirection == "L")
-			x--;
-		else if(xDirection == "R")
-			x++;
-		if(yDirection == "U")
-			y++;
-		else if(yDirection == "D")
-			y--;
-	}
-
-	public void draw () {
-		System.out.println("Kordinati na tockata se X: " + x + ", Y: "+ y);
-	}
-}
