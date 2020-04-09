76dd6e2055c0f5a7d23976302b2cf539511c49cc
NIario <63304806+NIario@users.noreply.github.com>
2020-04-09 18:59:25 +0200

    point class finished

--- Point.java
+++ Point.java
@@ -2,20 +2,57 @@ class Point {
 	String id
 	double x,y;
     String color;
-	//TODO add new variable
 
-	//TODO constructor
+	public Point(String id, double x, double y, String color){
+		this.id = id;
+		this.x = x;
+		this.y = y;
+		this.color = color;
+	}
 
-	//TODO setters and getters
+	public void setColor(String color) {
+		this.color = color;
+	}
 
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public String getId() {
+		return id;
+	}
 	public void move (char xDirection, char yDirection) {
-		//TODO
+		if(xDirection == "L")
+			x--;
+		else if(xDirection == "R")
+			x++;
+		if(yDirection == "U")
+			y++;
+		else if(yDirection == "D")
+			y--;
 	}
 
 	public void draw () {
-		//TODO
+		System.out.println("Kordinati na tockata se X: " + x + ", Y: "+ y);
 	}
-
-
-
 }
